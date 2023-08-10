f06dae46518cd251ce13acabc9f3914368c38fdd
Amir Pourmand <pourmand1376@gmail.com>
2023-08-10 14:59:22 +0330

    add menu

--- obsidian.sh
+++ obsidian.sh
@@ -1,22 +1,50 @@
 #!/bin/bash
 
-PS3='Please enter your choice: '
-options=("Option 1" "Option 2" "Option 3" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Option 1")
-            echo "You chose option 1"
-            ;;
-        "Option 2")
-            echo "You chose option 2" 
-            ;;
-        "Option 3")
-            echo "You chose option 3"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "Invalid option $REPLY";;
-    esac
+# Define functions for each menu option
+function list_files {
+    ls
+}
+
+function display_system_info {
+    uname -a
+}
+
+function list_processes {
+    ps -ef
+}
+
+# Main menu loop
+while true; do
+    PS3='Please enter your choice: '
+
+    options=(
+        "List files in current directory"
+        "Display system info" 
+        "List running processes"
+        "Quit"
+    )
+
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "${options[0]}")
+                list_files
+                break
+                ;;
+            "${options[1]}")
+                display_system_info
+                break
+                ;;
+            "${options[2]}")
+                list_processes
+                break
+                ;;
+            "${options[3]}")
+                exit 0
+                ;;
+            *) echo "Invalid option";;
+        esac
+    done
+
+    echo "-------------------------------------"
 done
