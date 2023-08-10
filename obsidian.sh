4ee1e297828617b40c56a6111015ec200c5e4ab6
Amir Pourmand <pourmand1376@gmail.com>
2023-08-10 22:26:31 +0330

    update obsidian

--- obsidian.sh
+++ obsidian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Script Version 0.3.2"
+echo "Script Version 0.3.3"
 echo "This script is used to facilitate configuration of git for obsidian. "
 
 HOME_PATH="/data/data/com.termux/files/home"
