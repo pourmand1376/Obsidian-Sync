d755270dda73a39ac8249ac0df88ec7fe77f9805
Amir Pourmand <pourmand1376@gmail.com>
2023-08-10 15:37:25 +0330

    add email and name

--- obsidian.sh
+++ obsidian.sh
@@ -14,9 +14,33 @@ function access_storage()
 termux-setup-storage
 }
 
-function list_processes {
-    ps -ef
+configure_git() {
+
+  name="$1"
+  email="$2"
+
+  git config --global user.name "$name"
+  git config --global user.email "$email"
+
+  git config --global credential.helper store
+  git config --global pull.rebase false
+  git config --global --add safe.directory '*'
+  git config --global core.protectNTFS false
+
 }
+
+generate_ssh_key() {
+  email="$1"
+  # Check if key already exists
+  if [ ! -f ~/.ssh/id_ed25519 ]; then
+    # Generate key non-interactively
+    ssh-keygen -q -t ed25519 -N "" -f ~/.ssh/id_ed25519 -C "$email" 
+    echo "Generated new SSH key with email $email"
+  else   
+    echo "SSH key already exists"
+  fi
+}
+
 
 # Main menu loop
 while true; do
@@ -25,7 +49,7 @@ while true; do
     options=(
         "Install Required Dependencies"
         "Give Access to Storage" 
-        "List running processes"
+        "Configure Git and Create SSH Key"
         "Quit"
     )
 
@@ -33,15 +57,38 @@ while true; do
     do
         case $opt in
             "${options[0]}")
+                echo "Installing Required packages"
                 install_required_deps
                 break
                 ;;
             "${options[1]}")
+                echo "Getting Access for Storage"
                 termux-setup-storage
                 break
                 ;;
             "${options[2]}")
-                list_processes
+                echo "Configuring Git and SSH Key"
+                while true; do
+                    read -p "Please Enter your name: " name
+                    if [[ -z "$name" ]]; then
+                        echo "Invalid input. Please enter a non-empty name."
+                    else
+                        echo "Your submitted name: $name"
+                        break
+                    fi
+                done
+                while true; do
+                    read -p "Please Enter your Email: " mail
+                    if [[ $mail =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$ ]]; then
+                        echo "Invalid input. Please enter a valid email."
+                    else
+                        echo "Your submitted email: $mail"
+                        break
+                    fi
+                done
+                echo "$name"
+                #configure_git
+                #generate_ssh_key
                 break
                 ;;
             "${options[3]}")
