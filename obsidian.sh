bb4714b449ea47a86046de34b19277d2d39db355
Amir Pourmand <pourmand1376@gmail.com>
2023-08-10 20:42:54 +0330

    add bash

--- obsidian.sh
+++ obsidian.sh
@@ -7,125 +7,125 @@ DOWNLOAD_FOLDER="$HOME_PATH/storage/shared/Download"
 # Define functions for each menu option
 function install_required_deps()
 {
-apt update
-apt upgrade -y
-pkg install openssh -y
-pkg install git -y
+    apt update
+    apt upgrade -y
+    pkg install openssh -y
+    pkg install git -y
 }
 
 function access_storage()
 {
-termux-setup-storage
+    termux-setup-storage
 }
 
 configure_git() {
 
-  name="$1"
-  email="$2"
+    name="$1"
+    email="$2"
 
-  git config --global user.name "$name"
-  git config --global user.email "$email"
+    git config --global user.name "$name"
+    git config --global user.email "$email"
 
-  git config --global credential.helper store
-  git config --global pull.rebase false
-  git config --global --add safe.directory '*'
-  git config --global core.protectNTFS false
-  git config --global core.longpaths true
+    git config --global credential.helper store
+    git config --global pull.rebase false
+    git config --global --add safe.directory '*'
+    git config --global core.protectNTFS false
+    git config --global core.longpaths true
 }
 
 generate_ssh_key() {
-  email="$1"
-  # Check if key already exists
-  if [ ! -f $HOME_PATH/.ssh/id_ed25519 ]; then
-    # Generate key non-interactively
-    ssh-keygen -q -t ed25519 -N "" -f $HOME_PATH/.ssh/id_ed25519 -C "$email" 
-    echo "Generated new SSH key with email $email"
-  else   
-    echo "SSH key already exists"
-  fi
-  echo "Here is your SSH public key. You can paste it inside Github"
-  echo "------------"
-  cat $HOME_PATH/.ssh/id_ed25519.pub
-  echo "------------"
-  eval $(ssh-agent -s)
-  ssh-add
+    email="$1"
+    # Check if key already exists
+    if [ ! -f $HOME_PATH/.ssh/id_ed25519 ]; then
+        # Generate key non-interactively
+        ssh-keygen -q -t ed25519 -N "" -f $HOME_PATH/.ssh/id_ed25519 -C "$email"
+        echo "Generated new SSH key with email $email"
+    else
+        echo "SSH key already exists"
+    fi
+    echo "Here is your SSH public key. You can paste it inside Github"
+    echo "------------"
+    cat $HOME_PATH/.ssh/id_ed25519.pub
+    echo "------------"
+    eval $(ssh-agent -s)
+    ssh-add
 }
 
 clone_repo() {
-  folder="$1"
-  git_url="$2"
-  echo "Git Folder: $HOME_PATH/$folder"
-  echo "Obsidian Folder: $DOWNLOAD_FOLDER/$folder"
-  echo "Git Url: $git_url"
+    folder="$1"
+    git_url="$2"
+    echo "Git Folder: $HOME_PATH/$folder"
+    echo "Obsidian Folder: $DOWNLOAD_FOLDER/$folder"
+    echo "Git Url: $git_url"
 
-  cd "$HOME_PATH/"
-  mkdir -p "$HOME_PATH/$folder"
+    cd "$HOME_PATH/"
+    mkdir -p "$HOME_PATH/$folder"
 
-  git --git-dir "$HOME_PATH/$folder" --work-tree "$DOWNLOAD_FOLDER/$folder" clone "$git_url"
-  cd "HOME_PATH/$folder"
-  git worktree add --checkout "$DOWNLOAD_FOLDER/$folder" --force 
+    git --git-dir "$HOME_PATH/$folder" --work-tree "$DOWNLOAD_FOLDER/$folder" clone "$git_url"
+    cd "HOME_PATH/$folder"
+    git worktree add --checkout "$DOWNLOAD_FOLDER/$folder" --force
 }
 
 # add gitignore file
 add_gitignore_entries() {
-folder_name="$1"
-cd "$DOWNLOAD_FOLDER/$folder_name"
-  GITIGNORE=".gitignore"
+    folder_name="$1"
+    cd "$DOWNLOAD_FOLDER/$folder_name"
+    GITIGNORE=".gitignore"
 
-  ENTRIES=".trash/ 
-.obsidian/workspace
-.obsidian/workspace.json
-.obsidian/workspace-mobile.json
-.obsidian/app.json"
+    ENTRIES=".trash/
+    .obsidian/workspace
+    .obsidian/workspace.json
+    .obsidian/workspace-mobile.json
+    .obsidian/app.json"
 
-  if [ ! -f "$GITIGNORE" ]; then
-    touch "$GITIGNORE"
-  fi
-
-  for entry in $ENTRIES; do
-    if ! grep -q -Fx "$entry" "$GITIGNORE"; then
-      echo "$entry" >> "$GITIGNORE"
+    if [ ! -f "$GITIGNORE" ]; then
+        touch "$GITIGNORE"
     fi
-  done
+
+    for entry in $ENTRIES; do
+        if ! grep -q -Fx "$entry" "$GITIGNORE"; then
+            echo "$entry" >> "$GITIGNORE"
+        fi
+    done
 
 }
 
 add_gitattributes_entry() {
-folder_name="$1"
-cd "$DOWNLOAD_FOLDER/$folder_name"
-  GITATTRIBUTES=".gitattributes"
-  ENTRY="*.md merge=union"
+    folder_name="$1"
+    cd "$DOWNLOAD_FOLDER/$folder_name"
+    GITATTRIBUTES=".gitattributes"
+    ENTRY="*.md merge=union"
 
-  if [ ! -f "$GITATTRIBUTES" ]; then
-    touch "$GITATTRIBUTES"
-  fi
+    if [ ! -f "$GITATTRIBUTES" ]; then
+        touch "$GITATTRIBUTES"
+    fi
 
-  if ! grep -q -F "$ENTRY" "$GITATTRIBUTES"; then
-    echo "$ENTRY" >> "$GITATTRIBUTES"
-  fi
+    if ! grep -q -F "$ENTRY" "$GITATTRIBUTES"; then
+        echo "$ENTRY" >> "$GITATTRIBUTES"
+    fi
 
 }
 
 remove_files_from_git()
 {
-folder_name="$1"
-cd "$DOWNLOAD_FOLDER/$folder_name"
+    folder_name="$1"
+    cd "$DOWNLOAD_FOLDER/$folder_name"
 
-FILES=".obsidian/workspace 
-.obsidian/workspace.json
-.obsidian/workspace-mobile.json
-.obsidian/app.json"
+    FILES=".obsidian/workspace
+    .obsidian/workspace.json
+    .obsidian/workspace-mobile.json
+    .obsidian/app.json"
 
-for file in $FILES; do
-  if [ -f "$file" ]; then
+    for file in $FILES; do
+        if [ -f "$file" ]; then
+            cd "$HOME_PATH/$folder_name"
+            git rm --cached "$file"
+        fi
+    done
     cd "$HOME_PATH/$folder_name"
-    git rm --cached "$file"
-  fi 
-done
-cd "$HOME_PATH/$folder_name"
-if git status | grep "new file" ; then
-  git commit -am "Remove ignored files"
-fi
+    if git status | grep "new file" ; then
+        git commit -am "Remove ignored files"
+    fi
 
 }
 
@@ -136,7 +136,7 @@ while true; do
 
     options=(
         "Install Required Dependencies"
-        "Give Access to Storage" 
+        "Give Access to Storage"
         "Configure Git and Create SSH Key"
         "Clone Obsidian Git Repo in Termux"
         "Optimize repository for multi-device use"
@@ -173,7 +173,7 @@ while true; do
                         echo "Your submitted email: $email"
                         break
                     else
-                         echo "Invalid input. Please enter a valid email."
+                        echo "Invalid input. Please enter a valid email."
                     fi
                 done
                 echo "-------------------------------------"
@@ -184,7 +184,7 @@ while true; do
             "${options[3]}")
                 echo "Cloning Obsidian Git Repo"
                 while true; do
-                echo "Please Enter your git url: "
+                    echo "Please Enter your git url: "
                     read git_url
                     if [[ -z "$git_url" ]]; then
                         echo "Invalid input. Please enter a non-empty git url."
@@ -230,7 +230,7 @@ while true; do
             "${options[5]}")
                 exit 0
                 ;;
-            *) echo "Invalid option";;
+            *) echo "Invalid option" ;;
         esac
     done
 
