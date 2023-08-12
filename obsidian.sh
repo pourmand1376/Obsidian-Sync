9df113c7be858cbe3a6e1c84799d43f3bf9c3762
a.pourmand <a.pourmand@emofid.com>
2023-08-12 13:59:17 +0330

    add obsidian shell

--- obsidian.sh
+++ obsidian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Script Version 0.4.1.0"
+echo "Script Version 0.4.1.2"
 echo "This script is used to facilitate configuration of git for obsidian. "
 
 HOME_PATH="/data/data/com.termux/files/home"
@@ -8,7 +8,7 @@ DOWNLOAD_FOLDER="$HOME_PATH/storage/shared/Download"
 OBSIDIAN_SCRIPT='
 function sync_obsidian()
 {
-	cd "$1"
+	cd "$1" { echo "Failure while changing directory into $1"; exit 1; }
 	git add .
 	git commit -m "Android Commit"
 	git fetch
